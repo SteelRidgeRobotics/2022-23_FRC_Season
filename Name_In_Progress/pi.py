d545f4a3cd1ab0e84c8671a565290dbbd3d420db
James <nwalker3.14159@gmail.com>
2023-02-02 15:43:16 -0800

    Working on image processing

--- Name_In_Progress/pi.py
+++ Name_In_Progress/pi.py
@@ -1,19 +1,30 @@
 from cscore import CameraServer
+from conepipeline import ConePipeline
 import cv2
-import numpy
+import time
 
 CameraServer.enableLogging()
 
 camera = CameraServer.startAutomaticCapture()
 camera.setResolution(320, 240)
 
-sink = CameraServer.getVideo()
+cvSink = CameraServer.getVideo()
 
-output = CameraServer.putVideo()
+output = CameraServer.putVideo("Camera 1", 320, 240)
 
 while True:
+    startTime = time.time()
     time, input_img = cvSink.grabFrame(input_img)
 
     if time == 0:
         continue
+
+    output_img = ConePipeline.process(input_img)
+
+    processingTime = time.time() - startTime
+    fps = 1/processingTime
+    cv2.putText(output_img, str(round(fps, 1)), (5, 50), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 255))
+
+    output.putFrame(output_img)
+
 
