1e232b70a5d5fff50fc312dfb7b91e050db83350
Pickle_Face5 <caden.dalley@icloud.com>
2023-02-14 17:37:16 -0800

    cadne u forgrt to push ur code -jmaes
    i have been through hell and back getting the wifi to work on this computer again, something weird happened and the computer got stuck in a restart loop
    
    desolate wasteland as far as the eye can see
    
    never again
    
    :+1:

---
+++ Project_Shrmip/constants.py
@@ -0,0 +1,9 @@
+
+# Motor stuff
+baseMotor = 4
+middleMotor = 5
+topMotor = 6
+graberMotor = 7
+wristMotor = 8
+
+ktimeoutMs = 10
---
+++ Project_Shrmip/robotcontainer.py
@@ -0,0 +1,94 @@
+import wpilib
+import commands2
+import constants
+import ctre
+from wpilib import XboxController
+from commands.drive_by_joystick import DriveByJoystick
+from commands.drive_straight import DriveStraight
+from subsystems.drivetrain import Drivetrain
+
+#hi
+class RobotContainer:
+    def __init__(self) -> None:
+        # driver controller
+        self.driverController = XboxController(constants.kdriverControllerPort)
+        
+        self.frontLeft = ctre.TalonFX(constants.kfrontLeft)
+        self.backLeft = ctre.TalonFX(constants.kbackLeft)
+        self.frontRight = ctre.TalonFX(constants.kfrontRight)
+        self.backRight = ctre.TalonFX(constants.kbackRight)
+
+
+        self.timer = wpilib.Timer
+        
+        
+        #subsystems
+        self.drive = Drivetrain()
+        
+        # chooser
+        self.chooser = wpilib.SendableChooser()
+        
+        # Add commands to autonomous command chooser
+        self.driveStraight = DriveStraight(self.drive, constants.kdistanceToTravel)
+        self.chooser.setDefaultOption("Drive Straight", self.driveStraight)
+
+        wpilib.SmartDashboard.putData("Autonomous", self.chooser)
+
+       
+
+        
+
+        
+                
+        #self.configureButtonBindings()  
+        
+        #self.drive.setDefaultCommand(DriveByJoystick(self.drive, lambda: -self.driverController.getLeftY(), lambda: -self.driverController.getRightY(), lambda: self.driverController.getRightBumper(), lambda: self.driverController.getLeftBumper()))
+        
+        
+        #self.drive.setDefaultCommand(DriveByJoystick(self.drive, lambda: -self.driverController.getLeftY(), lambda: -self.driverController.getRightY()))
+        
+        #ARCADE, OBJECTIVELY WAY BETTER - Pickle_Face5 & Wyatt
+        self.drive.setDefaultCommand(DriveByJoystick(self.drive, 
+        lambda: self.forwardSum(), 
+        lambda: self.reverseSum(), 
+        lambda: self.driverController.getRightBumper(), 
+        lambda: self.driverController.getLeftBumper()))
+
+    def forwardSum(self) -> float: # It took more than 2 and a half hours to get this to work, I swear if this stops working I'm going to commit a crime
+        leftY, rightX = self.addDeadZone(self.driverController.getLeftY(), self.driverController.getRightX())
+        finalValue = -leftY + rightX
+        if (finalValue > 1):
+            finalValue = 1
+        elif (finalValue < -1):
+            finalValue = -1
+        return finalValue
+
+    def reverseSum(self) -> float:
+        leftY, rightX = self.addDeadZone(self.driverController.getLeftY(), self.driverController.getRightX())
+        finalValue = -leftY - rightX
+        if (finalValue > 1):
+            finalValue = 1
+        elif (finalValue < -1):
+            finalValue = -1
+        return finalValue
+
+    def addDeadZone(self, leftYParam: float, rightXParam: float) -> tuple:
+        leftY = leftYParam
+        rightX = rightXParam
+        wpilib.SmartDashboard.putNumber('trueLeftJoy - ', leftY)
+        wpilib.SmartDashboard.putNumber('trueRightJoy - ', rightX)
+
+        # Controller Dead Zone
+        if (abs(leftY) <= constants.controllerDeadZoneLeft):
+            leftY = 0
+        if (abs(rightX) <= constants.controllerDeadZoneRight):
+            rightX = 0
+        return leftY, rightX
+
+        
+    #def configureButtonBindings(self):
+
+
+
+    def getAutonomousCommand(self) -> commands2.Command:
+        return self.chooser.getSelected()
