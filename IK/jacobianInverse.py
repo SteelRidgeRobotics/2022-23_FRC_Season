68e663f0bf14f1cf006ff7d9449b7aa0eafad69f
ProgramDragon64 <95453222+ProgramDragon64@users.noreply.github.com>
2023-02-21 17:57:09 -0800

    Working on limits
    Functional code, but there are bugs

--- IK/jacobianInverse.py
+++ IK/jacobianInverse.py
@@ -3,6 +3,7 @@ import math
 from robot_arm import *
 import pygame
 
+pygame.init()
 xOffset = 250
 yOffset = 250
 Arm = RobotArm(xBase=xOffset, yBase=yOffset)
@@ -18,6 +19,8 @@ window = pygame.display.set_mode((500,500))
 pygame.display.set_caption("Robot Arm Inverse Kinematics")
 window.fill((255,255,255))
 
+font = pygame.font.SysFont('lucidacalligraphy', 10)
+
 reach = sum(Arm.lengths)
 
 # add limits
@@ -25,13 +28,14 @@ Arm.add_limits()
 Arm.def_joint_limit(0, 0, math.pi)
 Arm.def_joint_limit(1, -math.pi, math.pi)
 Arm.def_joint_limit(2, -math.pi, math.pi)
+Arm.def_joint_limit(3, -math.pi, math.pi)
 
 def move_to_target():
     global Arm, target, reach
 
-    distPerUpdate = 0.01 * reach
+    distPerUpdate = 0.02 * reach
 
-    if np.linalg.norm(target - Arm.joints[:, [-1]]) > 0.01 * reach:
+    if np.linalg.norm(target - Arm.joints[:, [-1]]) > 0.02 * reach:
         targetVector = (target - Arm.joints[:, [-1]])[:3]
         targetUnitVector = targetVector / np.linalg.norm(targetVector)
         deltaR = distPerUpdate * targetUnitVector
@@ -41,13 +45,31 @@ def move_to_target():
         Arm.update_theta(deltaTheta)
         Arm.update_joint_coords()
 
+def get_limit_errors():
+    global Arm
+    angles = Arm.get_angles()
+    limitError = []
+    # check if arm is in its limits, makes a list that says errors, -1 for min error, 1 for max error, 0 for no error
+    for i in range(len(angles)):
+        # check if it is less than min
+        if angles[i] < Arm.limits[0][i]:
+            limitError.append(-1)
+            #thetaCheck = np.append(thetaCheck, Arm.limits[0][i])
+        # check if it is more than max
+        elif angles[i] > Arm.limits[1][i]:
+            limitError.append(1)
+            #thetaCheck = deltaTheta = np.append(deltaTheta, Arm.limits[1][i])
+        else:
+            limitError.append(0)
+    return limitError
+
 def move_to_target_with_limits():
     global Arm, target, reach
 
-    distPerUpdate = 0.01 * reach
+    distPerUpdate = 0.02 * reach
     angles = Arm.get_angles()
 
-    if np.linalg.norm(target - Arm.joints[:, [-1]]) > 0.01 * reach:
+    if np.linalg.norm(target - Arm.joints[:, [-1]]) > 0.02 * reach:
         targetVector = (target - Arm.joints[:, [-1]])[:3]
         targetUnitVector = targetVector / np.linalg.norm(targetVector)
         deltaR = distPerUpdate * targetUnitVector
@@ -55,24 +77,30 @@ def move_to_target_with_limits():
         JInv = np.linalg.pinv(J)
         deltaTheta = JInv.dot(deltaR)
 
-        limitError = []
-        # check if arm is in its limits, makes a list that says errors, -1 for min error, 1 for max error, 0 for no error
-        for i in range(len(angles)):
-            # check if it is less than min
-            if angles[i] < Arm.limits[0][i]:
-                limitError.append(-1)
-                limitError = np.append(limitError, )
-                #thetaCheck = np.append(thetaCheck, Arm.limits[0][i])
-            # check if it is more than max
-            elif angles[i] > Arm.limits[1][i]:
-                limitError.append(1)
-                #thetaCheck = deltaTheta = np.append(deltaTheta, Arm.limits[1][i])
-            else:
-                limitError.append(0)
-        # apply limits and check which need to be recalculated
-        redoneTheta = []
+        limitError = get_limit_errors()
+        limits = Arm.get_joint_limits()
+        redoneTheta = np.zeros((len(Arm.joints)), dtype=np.float_)
         for i in range(len(limitError)):
-            print()
+            # set the thetas to the limit
+            if limitError[i] == -1:
+                redoneTheta[i] = limits[0][i]
+            elif limitError[i] == 1:
+                redoneTheta[i] = limits[1][i]
+            else: # 0
+                # redo the jacobian matrix for the lengths that are not in error
+                #if i != (len(Arm.joints) - 1):
+                CJ = Arm.get_jacobian_with_specs(i, len(Arm.joints) - 1)
+                CJInv = np.linalg.pinv(CJ)
+                tempTheta = CJInv.dot(deltaR)
+                tempTheta = tempTheta.flatten()
+                redoneTheta[i] = tempTheta[0]
+                #else:
+            # go through deltaTheta and apply the new change we made.
+            for n in range(i):
+                deltaTheta[n][0] = redoneTheta[i]
+            # After each change we update it
+            Arm.update_theta(deltaTheta)
+            Arm.update_joint_coords()
 
         Arm.update_theta(deltaTheta)
         Arm.update_joint_coords()
@@ -83,7 +111,9 @@ def apply_limits_to_arm():
         angles = Arm.get_angles()
         J = Arm.get_jacobian()
         deltaTheta = []
-        # check if the angle of arm is above the max or below the min
+        
+        
+        # apply limits and check which need to be recalculated
         # we do this for each arm, hence the for loop
         for i in range(len(angles)):
             # check if it is less than min
@@ -123,9 +153,30 @@ while running:
     # Run IK code
     move_to_target()
 
+    limit0 = font.render(str(Arm.get_joint_limits()[0][0]) + " -> " + str(Arm.get_joint_limits()[1][0]), 1, (0,0,0))
+    limit1 = font.render(str(Arm.get_joint_limits()[0][1]) + " -> " + str(Arm.get_joint_limits()[1][1]), 1, (0,0,0))
+    limit2 = font.render(str(Arm.get_joint_limits()[0][2]) + " -> " + str(Arm.get_joint_limits()[1][2]), 1, (0,0,0))
+    limit3 = font.render(str(Arm.get_joint_limits()[0][3]) + " -> " + str(Arm.get_joint_limits()[1][3]), 1, (0,0,0))
+
+    window.blit(limit0, (0, 10))
+    window.blit(limit1, (0, 20))
+    window.blit(limit2, (0, 30))
+    window.blit(limit3, (0, 40))
+
+    angleDisplay = font.render(str(Arm.get_angles()), 1, (0,0,0))
+    window.blit(angleDisplay, (0, 0))
+
+    click = pygame.mouse.get_pos()
+    print(str(click))
+    targetPt = (click[0], click[1])
+    target = np.array([[click[0],click[1], 0, 1]]).T
+    print("TARGET: "+ str(target))
+    print(str(Arm.joints))
+    print("ANGLES: " + str(Arm.get_angles()))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            """
         if event.type == pygame.MOUSEBUTTONDOWN:
             click = pygame.mouse.get_pos()
             print(str(click))
@@ -135,6 +186,7 @@ while running:
             print(str(Arm.joints))
 
             print("ANGLES: " + str(Arm.get_angles()))
+            """
         
             
     pygame.display.flip()
