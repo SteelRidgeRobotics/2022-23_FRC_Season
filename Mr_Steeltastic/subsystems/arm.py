1c5cd1c3f4b7aa7db165394cc07e58930e241a44
ProgramDragon64 <95453222+ProgramDragon64@users.noreply.github.com>
2023-02-25 13:29:29 -0800

    Making Drivable

--- Mr_Steeltastic/subsystems/arm.py
+++ Mr_Steeltastic/subsystems/arm.py
@@ -37,43 +37,43 @@ class Arm(commands2.SubsystemBase):
         self.topMotor.configSelectedFeedbackSensor(ctre.FeedbackDevice.IntegratedSensor)
         self.grabberMotor.configSelectedFeedbackSensor(ctre.FeedbackDevice.IntegratedSensor)
         self.wristMotor.configSelectedFeedbackSensor(ctre.FeedbackDevice.QuadEncoder)
-        """
+        
         # set up motion magic
 
         # config PID values
-        self.baseMotor.config_kF() 
-        self.baseMotor.config_kP()
-        self.baseMotor.config_kD()
+        self.baseMotor.config_kF(constants.ARMBASEF) 
+        self.baseMotor.config_kP(constants.ARMBASEP)
+        self.baseMotor.config_kD(constants.ARMBASED)
 
-        self.midMotor.config_kF()
-        self.midMotor.config_kP()
-        self.midMotor.config_kD()
+        self.midMotor.config_kF(constants.ARMMIDF)
+        self.midMotor.config_kP(constants.ARMMIDP)
+        self.midMotor.config_kD(constants.ARMMIDD)
 
-        self.topMotor.config_kF()
-        self.topMotor.config_kP()
-        self.topMotor.config_kD()
+        self.topMotor.config_kF(constants.ARMTOPF)
+        self.topMotor.config_kP(constants.ARMTOPP)
+        self.topMotor.config_kD(constants.ARMTOPD)
 
-        self.grabberMotor.config_kF()
-        self.grabberMotor.config_kP()
-        self.grabberMotor.config_kD()
+        self.grabberMotor.config_kF(constants.ARMGRABBERF)
+        self.grabberMotor.config_kP(constants.ARMGRABBERP)
+        self.grabberMotor.config_kD(constants.ARMGRABBERD)
 
-        self.wristMotor.config_kF()
-        self.wristMotor.config_kP()
-        self.wristMotor.config_kD()
+        self.wristMotor.config_kF(constants.ARMWRISTF)
+        self.wristMotor.config_kP(constants.ARMWRISTP)
+        self.wristMotor.config_kD(constants.ARMWRISTD)
 
         # config cruising velocity
-        self.baseMotor.configMotionCruiseVelocity()
-        self.midMotor.configMotionCruiseVelocity()
-        self.topMotor.configMotionCruiseVelocity()
-        self.grabberMotor.configMotionCruiseVelocity()
-        self.wristMotor.configMotionCruiseVelocity()
+        self.baseMotor.configMotionCruiseVelocity(constants.ARMBASECRUISEVEL)
+        self.midMotor.configMotionCruiseVelocity(constants.ARMMIDCRUISEVEL)
+        self.topMotor.configMotionCruiseVelocity(constants.ARMTOPCRUISEVEL)
+        self.grabberMotor.configMotionCruiseVelocity(constants.ARMGRABBERCRUISEVEL)
+        self.wristMotor.configMotionCruiseVelocity(constants.ARMWRISTCRUISEVEL)
 
         # config acceleration
-        self.baseMotor.configMotionAcceleration()
-        self.midMotor.configMotionAcceleration()
-        self.topMotor.configMotionAcceleration()
-        self.grabberMotor.configMotionAcceleration()
-        self.wristMotor.configMotionAcceleration()
+        self.baseMotor.configMotionAcceleration(constants.ARMBASEMOTIONACCEL)
+        self.midMotor.configMotionAcceleration(constants.ARMMIDMOTIONACCEL)
+        self.topMotor.configMotionAcceleration(constants.ARMTOPMOTIONACCEL)
+        self.grabberMotor.configMotionAcceleration(constants.ARMGRABBERMOTIONACCEL)
+        self.wristMotor.configMotionAcceleration(constants.ARMWRISTMOTIONACCEL)
         
         # invert sensors
         self.baseMotor.setSensorPhase(False)
@@ -81,8 +81,6 @@ class Arm(commands2.SubsystemBase):
         self.topMotor.setSensorPhase(False)
         self.grabberMotor.setSensorPhase(False)
         self.wristMotor.setSensorPhase(False)
-        
-        """
 
         
         
@@ -91,7 +89,8 @@ class Arm(commands2.SubsystemBase):
         Move the arm to a specific pose.
         Requires angles for the base, middle, top, grabber, and wrist motors.
         """
-    
+        
+
     def setGrabber(self, bool):
         """
         Tell the grabber to open or close
