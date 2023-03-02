abe27ae05b1eea11ec95b9abaf4a2c57053acdb7
Steel Ridge Robotics <caden.dalley@icloud.com>
2023-03-01 17:24:41 -0800

    Added framework for ArmTest

--- Mr_Steeltastic/commands/armTest.py
+++ Mr_Steeltastic/commands/armTest.py
@@ -2,3 +2,10 @@ import commands2
 from subsystems.arm import Arm
 
 ## create command class that enters numbers into the HoldAtPercentage method of the Arm object.
+class ArmTest(commands2.CommandBase):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+
+
