be000906e1a0e773602de6535d6fc3fdaca007b7
ProgramDragon64 <95453222+ProgramDragon64@users.noreply.github.com>
2023-03-02 12:10:06 -0800

    Added functionality to armTest.py
    We will need to do a button binding in robotContainer.py

--- Mr_Steeltastic/commands/armTest.py
+++ Mr_Steeltastic/commands/armTest.py
@@ -3,8 +3,22 @@ from subsystems.arm import Arm
 
 ## create command class that enters numbers into the HoldAtPercentage method of the Arm object.
 class ArmTest(commands2.CommandBase):
-    def __init__(self) -> None:
+    def __init__(self, arm: Arm, base, mid, top, grabber) -> None:
         super().__init__()
+        self.arm = arm
+        
+        self.addRequirements([self.arm])
+    
+    def execute(self):
+        self.arm.HoldAtPercentage(base, mid, top, grabber)
+    
+    def end(self, interrupted):
+        self.arm.HoldAtPercentage(0.0, 0.0, 0.0, 0.0)
+    
+    def isFinished(self):
+        ## Will keep doing this command, unless we make a statement that will
+        ## return true once the command is "finished"
+        return False
 
 
 
