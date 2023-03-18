d2be73546dba47dd10416ae47d79b87b08501de1
James <cringeysketches@gmail.com>
2023-03-17 18:30:01 -0700

    gugrurrrrrhghghrh
    switched from numpy to math

--- Mr_Steeltastic/armSolver2.py
+++ Mr_Steeltastic/armSolver2.py
@@ -1,4 +1,4 @@
-import numpy as np
+import math
 
 class ArmSolver2():
     
@@ -7,20 +7,19 @@ class ArmSolver2():
         self.length1 = length1
         self.length2 = length2
     
-
     def targetToAngles(self, x, y):
         
         r = ((x ** 2) + (y ** 2)) ** 0.5
 
-        alpha = np.arccos((self.length1 ** 2) + (self.length2 ** 2) - r ** 2)
-        theta2 = np.pi - alpha
+        alpha = math.acos(((self.length1 ** 2) + (self.length2 ** 2) - r ** 2) / (2 * self.length1 * self.length2))
+        theta2 = math.radians(180) - alpha
 
-        psi = np.arctan2(self.length2 * np.sin(theta2), (self.length1 + self.length2 * np.cos(theta2)))
-        beta = np.arctan2(y, x)
+        psi = math.atan2(self.length2 * math.sin(theta2), (self.length1 + self.length2 * math.cos(theta2)))
+        beta = math.atan2(y, x)
 
         theta1 = beta - psi
 
-        return np.degrees(theta1), np.degrees(theta2)
+        return theta1, theta2
     
 as2 = ArmSolver2(100, 100)
 print(as2.targetToAngles(100, 100))
