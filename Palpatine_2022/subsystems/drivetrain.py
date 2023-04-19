a29d67e60b8d3003a780cead6601dbcfecf65812
Pickle_Face5 <caden.dalley@icloud.com>
2023-04-18 17:32:29 -0700

    Fixed Typo
    Technically this was James but he forgot to commit it lol

--- Palpatine_2022/subsystems/drivetrain.py
+++ Palpatine_2022/subsystems/drivetrain.py
@@ -2,12 +2,11 @@ import commands2
 import constants
 import ctre
 
-
 class Drivetrain(commands2.SubsystemBase):
     def __init__(self) -> None:
         super().__init__()
 
-        # initilize motors
+        # initialize motors
         self.frontLeft = ctre.TalonFX(constants.kfrontLeft)
         self.backLeft = ctre.TalonFX(constants.kbackLeft)
         self.frontRight = ctre.TalonFX(constants.kfrontRight)
